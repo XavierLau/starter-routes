6ba01e66568739f59e6676903ff1c4545f3f550f
inqr <>
2016-10-02 11:27:35 -0700

    closes issue #3 Signed-off-by:inqr

--- application/config/routes.php
+++ application/config/routes.php
@@ -63,3 +63,4 @@ $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['(^comp\d+)/:any'] = 'Wise/bingo';
+$route['sleep'] = 'first/zzz';
